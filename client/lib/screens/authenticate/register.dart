deaf48bf137eff4cde1f864d91055047faf3bbd4
usersina <benna.anis2@gmail.com>
2021-11-25 18:14:54 +0100

    Add loading & separate login/register functions

--- client/lib/screens/authenticate/register.dart
+++ client/lib/screens/authenticate/register.dart
@@ -4,6 +4,7 @@ import 'package:client/models/user.dart';
 import 'package:client/providers/user_provider.dart';
 import 'package:client/services/http_auth_service.dart';
 import 'package:client/shared/constants.dart';
+import 'package:client/shared/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -21,7 +22,7 @@ class _RegisterState extends State<Register> {
 
   final _formKey = GlobalKey<FormState>();
   bool _enableAutoValidation = false;
-  // bool loading = false;
+  bool loading = false;
 
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
@@ -35,124 +36,136 @@ class _RegisterState extends State<Register> {
       listen: false,
     );
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Create an account"),
-        actions: [
-          TextButton.icon(
-            onPressed: () {
-              widget.toggleView();
-            },
-            icon: const Icon(
-              Icons.person,
-              color: Colors.white,
-            ),
-            label: const Text(
-              "Sign in",
-              style: TextStyle(
-                color: Colors.white,
+    handleRegister() async {
+      setState(() {
+        _enableAutoValidation = true;
+      });
+      if (_formKey.currentState == null) return;
+
+      if (_formKey.currentState!.validate()) {
+        setState(() {
+          loading = true;
+        });
+
+        try {
+          User user = await _httpAuthService.registerWithEmailAndPassword(
+            _emailController.text,
+            _passwordController.text,
+          );
+          _userProvider.setNextStreamValue(user);
+        } catch (e) {
+          log(e.toString());
+          setState(() {
+            loading = false;
+          });
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text(
+                'Clould not register in with these credentials!',
+                style: TextStyle(fontSize: 16.0),
               ),
+              duration: Duration(seconds: 1),
+              backgroundColor: Colors.red,
             ),
-          )
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-          child: Form(
-            key: _formKey,
-            autovalidateMode: _enableAutoValidation
-                ? AutovalidateMode.onUserInteraction
-                : AutovalidateMode.disabled,
-            child: Column(
-              children: <Widget>[
-                const SizedBox(height: 20),
-                TextFormField(
-                  controller: _emailController,
-                  validator: (String? value) {
-                    if (value == null) return "";
+          );
+        }
+      }
+    }
 
-                    if (value.isEmpty || !value.contains("@")) {
-                      return "Please enter a valid email";
-                    }
-                    return null;
+    return loading
+        ? const Loading()
+        : Scaffold(
+            appBar: AppBar(
+              title: const Text("Create an account"),
+              actions: [
+                TextButton.icon(
+                  onPressed: () {
+                    widget.toggleView();
                   },
-                  decoration: textInputDecoration.copyWith(
-                    hintText: "Enter your email",
+                  icon: const Icon(
+                    Icons.person,
+                    color: Colors.white,
                   ),
-                ),
-                const SizedBox(height: 20),
-                TextFormField(
-                  controller: _passwordController,
-                  obscureText: true,
-                  validator: (String? value) {
-                    if (value == null) return "";
-
-                    if (value.isEmpty || value.length < 3) {
-                      return "Password should have 3 characters minimum";
-                    }
-                    return null;
-                  },
-                  decoration: textInputDecoration.copyWith(
-                    hintText: "Enter your password",
-                  ),
-                ),
-                const SizedBox(height: 20),
-                TextFormField(
-                  controller: _passwordConfirmController,
-                  obscureText: true,
-                  validator: (String? value) {
-                    if (value == null) return "";
-
-                    if (value.isEmpty || value != _passwordController.text) {
-                      return "Passwords do not match";
-                    }
-                    return null;
-                  },
-                  decoration: textInputDecoration.copyWith(
-                    hintText: "Confirm your password",
-                  ),
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () async {
-                    setState(() {
-                      _enableAutoValidation = true;
-                    });
-                    if (_formKey.currentState == null) return;
-
-                    if (_formKey.currentState!.validate()) {
-                      try {
-                        User user =
-                            await _httpAuthService.registerWithEmailAndPassword(
-                          _emailController.text,
-                          _passwordController.text,
-                        );
-                        _userProvider.setNextStreamValue(user);
-                      } catch (e) {
-                        log(e.toString());
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                            content: Text(
-                              'Clould not register in with these credentials!',
-                              style: TextStyle(fontSize: 16.0),
-                            ),
-                            duration: Duration(seconds: 1),
-                            backgroundColor: Colors.red,
-                          ),
-                        );
-                      }
-                    }
-                  },
-                  child: const Text(
-                    "Register",
+                  label: const Text(
+                    "Sign in",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
                   ),
                 )
               ],
             ),
-          ),
-        ),
-      ),
-    );
+            body: SingleChildScrollView(
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+                child: Form(
+                  key: _formKey,
+                  autovalidateMode: _enableAutoValidation
+                      ? AutovalidateMode.onUserInteraction
+                      : AutovalidateMode.disabled,
+                  child: Column(
+                    children: <Widget>[
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        controller: _emailController,
+                        validator: (String? value) {
+                          if (value == null) return "";
+
+                          if (value.isEmpty || !value.contains("@")) {
+                            return "Please enter a valid email";
+                          }
+                          return null;
+                        },
+                        decoration: textInputDecoration.copyWith(
+                          hintText: "Enter your email",
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        controller: _passwordController,
+                        obscureText: true,
+                        validator: (String? value) {
+                          if (value == null) return "";
+
+                          if (value.isEmpty || value.length < 3) {
+                            return "Password should have 3 characters minimum";
+                          }
+                          return null;
+                        },
+                        decoration: textInputDecoration.copyWith(
+                          hintText: "Enter your password",
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        controller: _passwordConfirmController,
+                        obscureText: true,
+                        validator: (String? value) {
+                          if (value == null) return "";
+
+                          if (value.isEmpty ||
+                              value != _passwordController.text) {
+                            return "Passwords do not match";
+                          }
+                          return null;
+                        },
+                        decoration: textInputDecoration.copyWith(
+                          hintText: "Confirm your password",
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: handleRegister,
+                        child: const Text(
+                          "Register",
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
   }
 }
--- client/lib/screens/authenticate/sign_in.dart
+++ client/lib/screens/authenticate/sign_in.dart
@@ -4,6 +4,7 @@ import 'package:client/models/user.dart';
 import 'package:client/providers/user_provider.dart';
 import 'package:client/services/http_auth_service.dart';
 import 'package:client/shared/constants.dart';
+import 'package:client/shared/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -21,10 +22,12 @@ class _SignInState extends State<SignIn> {
 
   final _formKey = GlobalKey<FormState>();
   bool _enableAutoValidation = false;
-  // bool loading = false;
+  bool loading = false;
 
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+
+  handleLogin() {}
 
   @override
   Widget build(BuildContext context) {
@@ -33,108 +36,118 @@ class _SignInState extends State<SignIn> {
       listen: false,
     );
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Sign in to get access"),
-        actions: [
-          TextButton.icon(
-            onPressed: () {
-              widget.toggleView();
-            },
-            icon: const Icon(
-              Icons.person,
-              color: Colors.white,
-            ),
-            label: const Text(
-              "Register",
-              style: TextStyle(
-                color: Colors.white,
+    handleSignIn() async {
+      setState(() {
+        _enableAutoValidation = true;
+      });
+      if (_formKey.currentState == null) return;
+
+      if (_formKey.currentState!.validate()) {
+        setState(() {
+          loading = true;
+        });
+        try {
+          User user = await _httpAuthService.signInWithEmailAndPassword(
+            _emailController.text,
+            _passwordController.text,
+          );
+          _userProvider.setNextStreamValue(user);
+        } catch (e) {
+          log(e.toString());
+          setState(() {
+            loading = false;
+          });
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text(
+                'Clould not log in with these credentials!',
+                style: TextStyle(fontSize: 16.0),
               ),
+              duration: Duration(seconds: 1),
+              backgroundColor: Colors.red,
             ),
-          )
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-          child: Form(
-            key: _formKey,
-            autovalidateMode: _enableAutoValidation
-                ? AutovalidateMode.onUserInteraction
-                : AutovalidateMode.disabled,
-            child: Column(
-              children: <Widget>[
-                const SizedBox(height: 20),
-                TextFormField(
-                  controller: _emailController,
-                  validator: (String? value) {
-                    if (value == null) return "";
+          );
+        }
+      }
+    }
 
-                    if (value.isEmpty || !value.contains("@")) {
-                      return "Please enter a valid email";
-                    }
-                    return null;
+    return loading
+        ? const Loading()
+        : Scaffold(
+            appBar: AppBar(
+              title: const Text("Sign in to get access"),
+              actions: [
+                TextButton.icon(
+                  onPressed: () {
+                    widget.toggleView();
                   },
-                  decoration: textInputDecoration.copyWith(
-                    hintText: "Enter your email",
+                  icon: const Icon(
+                    Icons.person,
+                    color: Colors.white,
                   ),
-                ),
-                const SizedBox(height: 20),
-                TextFormField(
-                  controller: _passwordController,
-                  obscureText: true,
-                  validator: (String? value) {
-                    if (value == null) return "";
-
-                    if (value.isEmpty || value.length < 3) {
-                      return "Password should have 3 characters minimum";
-                    }
-                    return null;
-                  },
-                  decoration: textInputDecoration.copyWith(
-                    hintText: "Enter your password",
-                  ),
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () async {
-                    setState(() {
-                      _enableAutoValidation = true;
-                    });
-                    if (_formKey.currentState == null) return;
-
-                    if (_formKey.currentState!.validate()) {
-                      try {
-                        User user =
-                            await _httpAuthService.signInWithEmailAndPassword(
-                          _emailController.text,
-                          _passwordController.text,
-                        );
-                        _userProvider.setNextStreamValue(user);
-                      } catch (e) {
-                        log(e.toString());
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                            content: Text(
-                              'Clould not log in with these credentials!',
-                              style: TextStyle(fontSize: 16.0),
-                            ),
-                            duration: Duration(seconds: 1),
-                            backgroundColor: Colors.red,
-                          ),
-                        );
-                      }
-                    }
-                  },
-                  child: const Text(
-                    "Sign In",
+                  label: const Text(
+                    "Register",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
                   ),
                 )
               ],
             ),
-          ),
-        ),
-      ),
-    );
+            body: SingleChildScrollView(
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+                child: Form(
+                  key: _formKey,
+                  autovalidateMode: _enableAutoValidation
+                      ? AutovalidateMode.onUserInteraction
+                      : AutovalidateMode.disabled,
+                  child: Column(
+                    children: <Widget>[
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        controller: _emailController,
+                        validator: (String? value) {
+                          if (value == null) return "";
+
+                          if (value.isEmpty || !value.contains("@")) {
+                            return "Please enter a valid email";
+                          }
+                          return null;
+                        },
+                        decoration: textInputDecoration.copyWith(
+                          hintText: "Enter your email",
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        controller: _passwordController,
+                        obscureText: true,
+                        validator: (String? value) {
+                          if (value == null) return "";
+
+                          if (value.isEmpty || value.length < 3) {
+                            return "Password should have 3 characters minimum";
+                          }
+                          return null;
+                        },
+                        decoration: textInputDecoration.copyWith(
+                          hintText: "Enter your password",
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: handleSignIn,
+                        child: const Text(
+                          "Sign In",
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
   }
 }
