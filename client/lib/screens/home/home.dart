a250e7f48070d74250f159048390e972e30fee69
usersina <benna.anis2@gmail.com>
2021-11-25 19:06:09 +0100

    Add buttons to home screen

--- client/lib/screens/home/home.dart
+++ client/lib/screens/home/home.dart
@@ -1,5 +1,7 @@
+import 'package:client/models/user.dart';
 import 'package:client/providers/user_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
@@ -7,6 +9,7 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<User?>(context);
     UserProvider _userProvider = Provider.of<UserProvider>(
       context,
       listen: false,
@@ -14,7 +17,10 @@ class Home extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home Screen"),
+        title: Text(
+          "Logged in as: ${user!.email}",
+          style: const TextStyle(fontSize: 17),
+        ),
         actions: [
           TextButton.icon(
             onPressed: () async {
@@ -31,13 +37,90 @@ class Home extends StatelessWidget {
           ),
         ],
       ),
-      body: const Center(
-        child: Text(
-          "All clear",
-          style: TextStyle(
-            fontSize: 26,
+      body: Column(
+        children: [
+          const SizedBox(height: 35),
+          Center(
+            child: Text(
+              "Select an action",
+              style: GoogleFonts.prozaLibre(
+                textStyle: const TextStyle(
+                  color: Colors.deepPurple,
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: .5,
+                ),
+              ),
+            ),
           ),
-        ),
+          const SizedBox(height: 25),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size.fromHeight(
+                  50,
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(Icons.group),
+                  SizedBox(width: 8),
+                  Text(
+                    'Manage Students',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ],
+              ),
+              onPressed: () {},
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size.fromHeight(
+                  50,
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(Icons.meeting_room),
+                  SizedBox(width: 8),
+                  Text(
+                    'Manage Classes',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ],
+              ),
+              onPressed: () {},
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size.fromHeight(
+                  50,
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(Icons.book_rounded),
+                  SizedBox(width: 8),
+                  Text(
+                    'Manage Courses',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ],
+              ),
+              onPressed: () {},
+            ),
+          ),
+        ],
       ),
     );
   }
